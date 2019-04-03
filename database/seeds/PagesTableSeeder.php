8d8f0911df7e5beba2cd97976f62593bbe811dce
Alex <herdadvance@gmail.com>
2019-04-02 22:03:39 -0400

    Show all pages

--- database/seeds/PagesTableSeeder.php
+++ database/seeds/PagesTableSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Page;
+
 class PagesTableSeeder extends Seeder
 {
     /**
@@ -11,6 +14,30 @@ class PagesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $admin = User::find(1);
+
+        Page::truncate();
+
+        $admin->pages()->saveMany([
+        	
+        	new Page([
+        		'title' => 'About',
+        		'url' => '/about',
+        		'content' => 'About our Company.'
+        	]),
+
+        	new Page([
+        		'title' => 'Team',
+        		'url' => '/team',
+        		'content' => 'Our awesome staff.'
+        	]),
+
+        	new Page([
+        		'title' => 'Contact',
+        		'url' => '/contact',
+        		'content' => 'Drop us a line.'
+        	]),
+        	
+        ]);
     }
 }
--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -1,1 +1,7 @@
-<h1>Index</h1>
+<h1>Pages</h1>
+
+<a href="{{ route('pages.create') }}">Create New Page</a>
+
+@foreach($pages as $page)
+	<p><a href="{{$page->url}}">{{$page->title}}</a></p>
+@endforeach
